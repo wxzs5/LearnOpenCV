f443b521585cf12f2e06baf81d9b2da4c1c5ecee
wxzs5 <790230517@qq.com>
2017-10-24 20:57:54 +0800

    Image Pyramid

---
+++ ImagePyramid.py
@@ -0,0 +1,53 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+# img=cv2.imread('messi5.jpg')
+# res=cv2.pyrDown(img);
+# cv2.imshow('pyradown',res)
+# cv2.imshow('pre',img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+#####Image Blending
+A = cv2.imread('apple.jpg')
+B = cv2.imread('orange.jpg')
+# generate Gaussian pyramid for A
+rows,cols,ch=B.shape
+G = A[0:rows,0:cols,:]
+gpA = [G]
+for i in range(6):
+    G = cv2.pyrDown(G)
+    gpA.append(G)
+# generate Gaussian pyramid for B
+G = B.copy()
+gpB = [G]
+for i in range(6):
+    G = cv2.pyrDown(G)
+    gpB.append(G)
+# generate Laplacian Pyramid for A
+lpA = [gpA[5]]
+for i in range(5,0,-1):
+    GE = cv2.pyrUp(gpA[i])
+    L = cv2.subtract(gpA[i-1],GE)
+    lpA.append(L)
+# generate Laplacian Pyramid for B
+lpB = [gpB[5]]
+for i in range(5,0,-1):
+    GE = cv2.pyrUp(gpB[i])
+    L = cv2.subtract(gpB[i-1],GE)
+    lpB.append(L)
+# Now add left and right halves of images in each level
+LS = []
+for la,lb in zip(lpA,lpB):
+    rows,cols,dpt = la.shape
+    ls = np.hstack((la[:,0:cols/2], lb[:,cols/2:]))
+    LS.append(ls)
+# now reconstruct
+ls_ = LS[0]
+for i in range(1,6):
+    ls_ = cv2.pyrUp(ls_)
+    ls_ = cv2.add(ls_, LS[i])
+# image with direct connecting each half
+real = np.hstack((A[:,:cols/2],B[:,cols/2:]))
+
