016f27644b253d22815ff2ca1311bb18f4bceaa7
wxzs5 <790230517@qq.com>
2017-11-01 17:56:09 +0800

    Histogram start

---
+++ Histogram.py
@@ -0,0 +1,42 @@
+# #####Using Matplotlib
+# import cv2
+# import numpy as np
+# from matplotlib import pyplot as plt
+
+# img=cv2.imread('res/apple.jpg',0)
+# plt.hist(img.ravel(),256,[0,256])
+# plt.show()
+
+# #####Draw every channel
+# import cv2
+# import numpy as np
+# from matplotlib import pyplot as plt
+
+# img=cv2.imread('res/apple.jpg')
+# color=('b','g','r')
+# for i,col in enumerate(color):
+#     histr=cv2.calcHist([img],[i],None,[256],[0,256])
+#     plt.plot(histr,color=col)
+#     plt.xlim([0,255])
+# plt.show()
+
+#####Using OpenCV
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img=cv2.imread('res/apple.jpg',0)
+#creat a mask
+mask=np.zeros(img.shape[:2],np.uint8)
+mask[20:220,20:220]=255
+masked_img=cv2.bitwise_and(img,img,mask=mask)
+#Calculate histogram with/without mask
+hist_full=cv2.calcHist([img],[0],None,[256],[0,256])
+hist_mask=cv2.calcHist([img],[0],mask,[256],[0,256])
+
+plt.subplot(221),plt.imshow(img,'gray')
+plt.subplot(222),plt.imshow(mask,'gray')
+plt.subplot(223),plt.imshow(masked_img,'gray')
+plt.subplot(224),plt.plot(hist_full),plt.plot(hist_mask),plt.xlim([0,256])
+plt.show()
+
