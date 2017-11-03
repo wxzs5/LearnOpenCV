9eb01ddcb19e6189f56280bcb6aa4ac5fc1429a8
wxzs5 <790230517@qq.com>
2017-11-03 19:17:23 +0800

    2D Histogram

---
+++ 2DHistograms.py
@@ -0,0 +1,23 @@
+#####OpenCV method
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img=cv2.imread('res/home.jpg')
+hsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+hist=cv2.calcHist([hsv],[0,1],None,[180,256],[0,180,0,256])
+
+plt.imshow(hist,interpolation='nearest')
+plt.show()
+
+# #####2D Histogram in Numpy
+# import cv2
+# import numpy as np
+# from matplotlib import pyplot as plt
+
+# img=cv2.imread('res/home.jpg')
+# hsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+
+# hist,xbins,ybins=np.histogram2d(h.ravel(),s.ravel(),[180,256],[0,180],[0,256])
+# plt.imshow(hist,interpolation = 'nearest')
+# plt.show()
