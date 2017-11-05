6144a5f5d0bfa77a9fdb6f6989cba011f78c19a0
wxzs5 <790230517@qq.com>
2017-11-05 18:50:38 +0800

    Hough Lines

---
+++ HoughLines.py
@@ -0,0 +1,38 @@
+# #####Hough Line Transform
+# import cv2
+# import numpy as np
+
+# img = cv2.imread('res/sudoku.png')
+# gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+# edges = cv2.Canny(gray,50,150,apertureSize = 3)
+# lines = cv2.HoughLines(edges,1,np.pi/180,130)
+# for line in lines:
+#     rho,theta = line[0]
+#     a = np.cos(theta)
+#     b = np.sin(theta)
+#     x0 = a*rho
+#     y0 = b*rho
+#     x1 = int(x0 + 1000*(-b))
+#     y1 = int(y0 + 1000*(a))
+#     x2 = int(x0 - 1000*(-b))
+#     y2 = int(y0 - 1000*(a))
+#     cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
+
+# cv2.imshow('houghlines3',img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+#####Probabilistic Hought Line Transform
+import cv2
+import numpy as np
+img = cv2.imread('res/sudoku.png')
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+edges = cv2.Canny(gray,50,150,apertureSize = 3)
+lines = cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength=100,maxLineGap=10)
+for line in lines:
+    x1,y1,x2,y2 = line[0]
+    cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
+cv2.imshow('houghlines5',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
