5efc28875d04c3fadd691e29db85b452d3d4af5e
wxzs5 <790230517@qq.com>
2017-11-26 16:46:35 +0800

    SURF

---
+++ SURF.py
@@ -0,0 +1,7 @@
+#SURF is 3 times faster than SIFT while performance is comparable to SIFT.
+#SURF is good at handling images with blurring and rotation, but not good at handling viewpoint change and illumination change.
+
+
+#No proper demo->_-> Referring to API instructon
+
+
