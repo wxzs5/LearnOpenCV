a15659a7397bc81f970a97220ec5ff25a0419ee0
wxzs5 <790230517@qq.com>
2017-12-01 20:37:14 +0800

    FAST Algorithm

---
+++ FAST.py
@@ -0,0 +1,31 @@
+#This algorithm is faster than others,but need a proper threshold&not robust to noise
+
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+img = cv2.imread('res/sudoku.png',0)
+
+# Initiate FAST object with default values
+fast = cv2.FastFeatureDetector_create()
+
+# find and draw the keypoints
+kp = fast.detect(img,None)
+img2 = cv2.drawKeypoints(img, kp, None, color=(255,0,0))
+
+# Print all default params
+print( "Threshold: {}".format(fast.getThreshold()) )
+print( "nonmaxSuppression:{}".format(fast.getNonmaxSuppression()) )
+print( "neighborhood: {}".format(fast.getType()) )
+print( "Total Keypoints with nonmaxSuppression: {}".format(len(kp)) )
+
+# Disable nonmaxSuppression
+fast.setNonmaxSuppression(0)
+kp = fast.detect(img,None)
+print( "Total Keypoints without nonmaxSuppression: {}".format(len(kp)) )
+img3 = cv2.drawKeypoints(img, kp, None, color=(255,0,0))
+
+cv2.imshow('fast_true',img2)
+cv2.imshow('Result',img3)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
