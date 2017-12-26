bf25c87e68861a4f42e778e1d77a15d398e5605e
wxzs5 <790230517@qq.com>
2017-12-26 19:14:15 +0800

    Regular and add blox.jpg

--- ORB.py
+++ ORB.py
@@ -1,16 +1,16 @@
 #It's very suit for SLAM
-import numpy as np
-import cv2
+import numpy as np 
+import cv2 as cv
 from matplotlib import pyplot as plt
-img = cv2.imread('res/chess.bmp',0)
+img = cv.imread('res/blox.jpg',0)
 # Initiate ORB detector
-orb = cv2.ORB_create()
+orb = cv.ORB_create()
 # find the keypoints with ORB
 kp = orb.detect(img,None)
 # compute the descriptors with ORB
 kp, des = orb.compute(img, kp)
 # draw only keypoints location,not size and orientation
-img2 = cv2.drawKeypoints(img, kp, None, color=(0,255,0), flags=0)
+img2 = cv.drawKeypoints(img, kp, None, color=(0,255,0), flags=0)
 plt.subplot(121),plt.imshow(img),plt.title('Source'),plt.xticks([]),plt.yticks([])
 plt.subplot(122),plt.imshow(img2),plt.title('ORB'),plt.xticks([]),plt.yticks([])
 plt.show()
